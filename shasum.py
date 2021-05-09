0d0e2e18fb5deed22d274aa416f150b26dcf55c4
kantsuw <kantsuwan@gmail.com>
2021-05-09 14:12:01 +0700

    Update shasum.py

--- shasum.py
+++ shasum.py
@@ -9,6 +9,7 @@ sha256 = hashlib.sha256()
 headers = {'User-Agent': 'curl/7.64.1'}
 url = 'https://download.litecoin.org/litecoin-0.18.1/linux/litecoin-0.18.1-linux-signatures.asc'
 
+#Download file from "url" then get shasum from file
 def get_shasum(filename):
   with open(filename, 'rb') as f:
       while True:
@@ -17,7 +18,6 @@ def get_shasum(filename):
               break
           sha256.update(data)
   return sha256.hexdigest()
-
 request = urllib.request.Request(url,None,headers)
 response = urllib.request.urlopen(request)
 data = response.read().decode("utf-8")
@@ -25,6 +25,8 @@ file = open('/tmp/litecoin-signatures.asc','w')
 file.write(data)
 file.close()
 
+
+#Compare SHASUM and print result
 with open('/tmp/litecoin-signatures.asc') as f:
   for line in f:
     if 'litecoin-0.18.1-x86_64-linux-gnu.tar.gz' in line:
